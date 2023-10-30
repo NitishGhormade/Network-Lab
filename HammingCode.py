2955d49ccb3ae2f20d1ef8a0f2f7462ab507b5b4
NitishGhormade <118716634+NitishGhormade@users.noreply.github.com>
2023-10-30 15:57:00 +0530

    HammingCode.py

--- HammingCode.py
+++ HammingCode.py
@@ -1,9 +1,26 @@
-data = [x for x in range(5)]
-m = len(data)
+mdata = input("Enter the Data: ")
+parity = int(input("Enter 1 for EVEN PARITY and 0 for ODD PARITY: "))
+m = len(mdata)
 r = 0
-for rcheck in range(len(data)):
+for rcheck in range(len(mdata)):
     if(2**rcheck >= m + rcheck + 1):
         r = rcheck
         break
 
-print(r)
+hammingCode = []
+
+p = [x for x in range(m+r) if(2**x <= m+r)]
+
+counter = 0
+for i in range(1,m+r+1):
+    if(i in p):
+        hammingCode.append(-1)
+    else:
+        hammingCode.append(mdata[counter])
+        counter += 1
+
+for pow in p:
+    s = []
+    for idx in range(pow-1,m+r,2*pow):
+        s = hammingCode[idx:idx + pow]
+
